c51d4115c2759164967b1ae840dce61477401d36
Sergei Gannochenko <s.gannochenko@c02c180mmd6t.ad.deliveryhero.com>
2025-01-17 16:09:44 +0100

    wip

--- dist/commands/type.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Implements = Implements;
-function Implements() {
-    return (constructor) => {
-        // eslint-disable-next-line no-unused-expressions
-        constructor;
-    };
-}
