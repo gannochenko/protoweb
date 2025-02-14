4f79b2b08bcfb4974cff6ccfb51047bfefc67ab4
Sergei Gannochenko <s.gannochenko@c02c180mmd6t.ad.deliveryhero.com>
2025-02-14 15:02:45 +0100

    wip

--- scripts/testrun.sh
+++ scripts/testrun.sh
@@ -4,11 +4,3 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 rm -rf ${DIR}/../generated/*
 yarn start build -i ${HOME}/proj/proto/supplier_portal/ -o ${DIR}/../generated -r /Users/s.gannochenko/proj/proto --with-json-decoder --with-json-decoder-required-fields
-
-
-
-protoweb build -i ${HOME}/dh-nv-proto/supplier_portal/ -o ./generated -r ${HOME}/dh-nv-proto/proto \
-  --with-json-decoder --with-json-decoder-required-fields \
-  --with-json-decoder-ignore-files *google/protobuf/descriptor.proto*,*google/api*
-
-
